6d2a218e84bb6f3323a8d4cd9dd01955bc488c07
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-14 04:40:12 -0300

    test: create new invalid planet

--- src/test/java/com/example/demo/domain/PlanetServiceTest.java
+++ src/test/java/com/example/demo/domain/PlanetServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.demo.domain;
 
 import static com.example.demo.common.PlanetConstants.PLANET;
+import static com.example.demo.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -55,5 +57,15 @@ public class PlanetServiceTest {
 
     // ASSERT
     assertThat(sut).isEqualTo(PLANET);
+  }
+
+  @Test
+  public void createPlanet_WithInvalidData_ThrowsException() {
+
+    when(planetRepository.save(INVALID_PLANET))
+        .thenThrow(RuntimeException.class);
+
+    assertThatThrownBy(() -> planetService.create(INVALID_PLANET))
+        .isInstanceOf(RuntimeException.class);
   }
 }
