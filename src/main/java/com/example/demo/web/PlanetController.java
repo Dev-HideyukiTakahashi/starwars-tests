54739c622d489dcaf400f6bec2d7b780737dd12d
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-14 02:49:34 -0300

    fix: create uri location

--- src/main/java/com/example/demo/web/PlanetController.java
+++ src/main/java/com/example/demo/web/PlanetController.java
@@ -1,12 +1,14 @@
 package com.example.demo.web;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.domain.Planet;
 import com.example.demo.domain.PlanetService;
@@ -21,7 +23,12 @@ public class PlanetController {
   @PostMapping
   public ResponseEntity<Planet> create(@RequestBody Planet entity) {
     Planet planetCreated = planetService.create(entity);
-    return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
+    URI uri = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(planetCreated.getId())
+        .toUri();
+    return ResponseEntity.created(uri).body(planetCreated);
   }
 
 }
